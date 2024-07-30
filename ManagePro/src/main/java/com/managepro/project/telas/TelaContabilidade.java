85815fe2c0757e83da9930617f318449911eb4b4
BrenoMontenegro <breno.freitas1608@gmail.com>
2024-07-29 22:13:42 -0300

    mudanca na tela visual e nomeacao das variaveis

--- ManagePro/src/main/java/com/managepro/project/telas/TelaContabilidade.java
+++ ManagePro/src/main/java/com/managepro/project/telas/TelaContabilidade.java
@@ -18,19 +18,20 @@ import javax.swing.JOptionPane;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.JButton;
 import javax.swing.ImageIcon;
+import java.util.Date;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import com.toedter.calendar.JDateChooser;
 
 public class TelaContabilidade extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
 	private JLabel lblImage1;		//IMAGENS DOS GRÁFICOS (APAGAR DEPOIS: APENAS TESTE)
 	private JLabel lblImage2;		//AVISO: NOMES GENÉRICOS CRIADOS PROPOSITALMENTE: APENAS TESTE
 	private JLabel lblImage3;
-	private JLabel lblImage4;
+	private JDateChooser dateChooser;
+	private JDateChooser dateChooser_1;
 
 	/**
 	 * Launch the application.
@@ -56,108 +57,152 @@ public class TelaContabilidade extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1020, 600);
 		contentPane = new JPanel();
+		contentPane.setBackground(new Color(192, 192, 192));
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		Panel panel = new Panel();
-		panel.setBackground(new Color(109, 109, 109));
-		panel.setBounds(132, 175, 458, 192);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		Panel painelGrafico = new Panel();
+		painelGrafico.setBackground(new Color(255, 255, 255));
+		painelGrafico.setBounds(132, 230, 510, 150);
+		contentPane.add(painelGrafico);
+		painelGrafico.setLayout(null);
 		
-		Panel panel_1 = new Panel();
-		panel_1.setBackground(new Color(109, 109, 109));
-		panel_1.setBounds(608, 175, 260, 192);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
+		Panel painelGrafico2 = new Panel();
+		painelGrafico2.setBackground(new Color(255, 255, 255));
+		painelGrafico2.setBounds(648, 230, 249, 300);
+		contentPane.add(painelGrafico2);
+		painelGrafico2.setLayout(null);
 		
-		Panel panel_2 = new Panel();
-		panel_2.setBackground(new Color(128, 128, 128));
-		panel_2.setBounds(0, 38, 1004, 129);
-		contentPane.add(panel_2);
-		panel_2.setLayout(null);
+		Panel painelOpcoes = new Panel();
+		painelOpcoes.setBackground(new Color(153, 51, 153));
+		painelOpcoes.setBounds(0, 0, 1004, 93);
+		contentPane.add(painelOpcoes);
+		painelOpcoes.setLayout(null);
 		
-		JLabel lblNewLabel_1 = new JLabel("Data Inicial *");
-		lblNewLabel_1.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		lblNewLabel_1.setBounds(62, 28, 101, 29);
-		panel_2.add(lblNewLabel_1);
+		JLabel dataLabel = new JLabel("Data Inicial *");
+		dataLabel.setForeground(new Color(255, 255, 255));
+		dataLabel.setBackground(new Color(255, 255, 255));
+		dataLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		dataLabel.setBounds(72, 11, 101, 29);
+		painelOpcoes.add(dataLabel);
 		
-		JLabel lblNewLabel_1_1 = new JLabel("Data Final *");
-		lblNewLabel_1_1.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		lblNewLabel_1_1.setBounds(266, 28, 75, 29);
-		panel_2.add(lblNewLabel_1_1);
+		JLabel finalLabel = new JLabel("Data Final *");
+		finalLabel.setForeground(new Color(255, 255, 255));
+		finalLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		finalLabel.setBounds(289, 11, 75, 29);
+		painelOpcoes.add(finalLabel);
 		
-		JLabel lblNewLabel_1_1_1 = new JLabel("Agrupar por *");
-		lblNewLabel_1_1_1.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		lblNewLabel_1_1_1.setBounds(469, 28, 101, 29);
-		panel_2.add(lblNewLabel_1_1_1);
+		JLabel agruparLabel = new JLabel("Agrupar por *");
+		agruparLabel.setForeground(new Color(255, 255, 255));
+		agruparLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		agruparLabel.setBounds(507, 11, 101, 29);
+		painelOpcoes.add(agruparLabel);
 		
 		JComboBox comboBox = new JComboBox();
-		comboBox.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Venda", "Produto", "Cliente", "Forma de pagamento"}));
-		comboBox.setBounds(469, 54, 167, 35);
-		panel_2.add(comboBox);
+		comboBox.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		comboBox.setModel(new DefaultComboBoxModel(new String[] {"Venda", "Produto", "Cliente", "Forma de pagamento", "Código"}));
+		comboBox.setBounds(507, 39, 167, 35);
+		painelOpcoes.add(comboBox);
 		
-		textField = new JTextField();
-		textField.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		textField.setBounds(62, 54, 155, 35);
-		panel_2.add(textField);
-		textField.setColumns(10);
+		JButton gerarRelatorio = new JButton("Gerar relatório");
+		gerarRelatorio.setBackground(new Color(255, 255, 255));
+		gerarRelatorio.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		gerarRelatorio.setBounds(749, 41, 145, 31);
+		painelOpcoes.add(gerarRelatorio);
 		
-		textField_1 = new JTextField();
-		textField_1.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		textField_1.setColumns(10);
-		textField_1.setBounds(266, 54, 155, 35);
-		panel_2.add(textField_1);
+		dateChooser = new JDateChooser();
+		dateChooser.setBounds(72, 39, 155, 35);
+		painelOpcoes.add(dateChooser);
 		
-		JButton btnNewButton = new JButton("Gerar relatório");
-		btnNewButton.setBackground(new Color(255, 255, 255));
-		btnNewButton.setFont(new Font("SansSerif", Font.PLAIN, 14));
-		btnNewButton.setBounds(735, 57, 145, 31);
-		panel_2.add(btnNewButton);
-		btnNewButton.addActionListener(new ActionListener() {
+		dateChooser_1 = new JDateChooser();
+		dateChooser_1.setBounds(289, 39, 161, 35);
+		painelOpcoes.add(dateChooser_1);
+		
+		//BOTÃO
+		gerarRelatorio.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				validarTexto();
+				validarData();
 			}
 		});	
 		
 		
-		Panel panel_3 = new Panel();
-		panel_3.setBackground(new Color(109, 109, 109));
-		panel_3.setBounds(132, 380, 360, 171);
-		contentPane.add(panel_3);
-		panel_3.setLayout(null);
+		Panel painelGrafico3 = new Panel();
+		painelGrafico3.setBackground(new Color(255, 255, 255));
+		painelGrafico3.setBounds(132, 386, 510, 144);
+		contentPane.add(painelGrafico3);
+		painelGrafico3.setLayout(null);
 		
-		Panel panel_3_1 = new Panel();
-		panel_3_1.setBackground(new Color(109, 109, 109));
-		panel_3_1.setBounds(508, 380, 360, 171);
-		contentPane.add(panel_3_1);
-		panel_3_1.setLayout(null);
+		Panel cardUm = new Panel();
+		cardUm.setBackground(new Color(255, 255, 255));
+		cardUm.setBounds(132, 108, 255, 116);
+		contentPane.add(cardUm);
+		cardUm.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("Produtos");
+		lblNewLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		lblNewLabel.setBounds(20, 11, 60, 14);
+		cardUm.add(lblNewLabel);
+		
+		JLabel lblNewLabel_2 = new JLabel("340");
+		lblNewLabel_2.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabel_2.setBounds(151, 68, 60, 26);
+		cardUm.add(lblNewLabel_2);
 		
 		
 		lblImage1 = new JLabel("fotoaqui");
 		lblImage1.setBounds(10, 11, 438, 170);
-		panel.add(lblImage1);
+		painelGrafico.add(lblImage1);
 		
 		lblImage2 = new JLabel("fotoaqui");
-		lblImage2.setBounds(10, 11, 438, 170);
-		panel_1.add(lblImage2);
+		lblImage2.setBounds(15, 11, 208, 265);
+		painelGrafico2.add(lblImage2);
 		
 		lblImage3 = new JLabel("fotoaqui");
-		lblImage3.setBounds(10, 11, 438, 170);
-		panel_3.add(lblImage3);
+		lblImage3.setBounds(10, -21, 438, 170);
+		painelGrafico3.add(lblImage3);
 		
-		lblImage4 = new JLabel("fotoaqui");
-		lblImage4.setBounds(10, 11, 438, 170);
-		panel_3_1.add(lblImage4);
+		Panel cardDois = new Panel();
+		cardDois.setLayout(null);
+		cardDois.setBackground(new Color(255, 255, 255));
+		cardDois.setBounds(394, 108, 248, 116);
+		contentPane.add(cardDois);
+		
+		JLabel lblNewLabel_5 = new JLabel("Vendas");
+		lblNewLabel_5.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		lblNewLabel_5.setBounds(20, 11, 46, 14);
+		cardDois.add(lblNewLabel_5);
+		
+		JLabel lblNewLabel_2_1 = new JLabel("340");
+		lblNewLabel_2_1.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabel_2_1.setBounds(169, 68, 60, 26);
+		cardDois.add(lblNewLabel_2_1);
+		
+		Panel cardTres = new Panel();
+		cardTres.setLayout(null);
+		cardTres.setBackground(new Color(255, 255, 255));
+		cardTres.setBounds(648, 108, 249, 116);
+		contentPane.add(cardTres);
+		
+		JLabel lblNewLabel_3 = new JLabel("Ganhos");
+		lblNewLabel_3.setFont(new Font("SansSerif", Font.PLAIN, 12));
+		lblNewLabel_3.setBounds(26, 11, 46, 14);
+		cardTres.add(lblNewLabel_3);
+		
+		JLabel lblNewLabel_4 = new JLabel("R$ 12.500");
+		lblNewLabel_4.setFont(new Font("SansSerif", Font.PLAIN, 20));
+		lblNewLabel_4.setBounds(104, 68, 111, 26);
+		cardTres.add(lblNewLabel_4);
 		
 	}
 	
 	
-		private void validarTexto() { //VERIFICA SE A CAIXA DE TEXTO FOI DEIXADA VAZIA
-			if (textField.getText().isEmpty() || textField_1.getText().isEmpty()) {
+		private void validarData() { 
+			Date dataInicial = dateChooser.getDate();
+			Date dataFinal = dateChooser_1.getDate();
+			
+			if(dataInicial == null || dataFinal == null) {
 				JOptionPane.showMessageDialog(this, "Por favor, preencha todos os campos obrigatórios.", "Erro", JOptionPane.ERROR_MESSAGE);
 			} else {
 				showImage();
@@ -168,9 +213,5 @@ public class TelaContabilidade extends JFrame {
 			lblImage1.setIcon(new ImageIcon(TelaContabilidade.class.getResource("/images/imagem-grafico.png")));
 			lblImage2.setIcon(new ImageIcon(TelaContabilidade.class.getResource("/images/imagem-grafico.png")));
 			lblImage3.setIcon(new ImageIcon(TelaContabilidade.class.getResource("/images/imagem-grafico.png")));
-			lblImage4.setIcon(new ImageIcon(TelaContabilidade.class.getResource("/images/imagem-grafico.png")));
     }
-		
-		
-
 }
