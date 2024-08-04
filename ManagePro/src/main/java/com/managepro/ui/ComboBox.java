c7dd168b3cb5e5a8f14a7bee077a793546483b98
CaioCesarMDS <caiocesarsts@gmail.com>
2024-08-04 20:38:48 -0300

    mudança da classe label para combobox

--- ManagePro/src/main/java/com/managepro/ui/ComboBox.java
+++ ManagePro/src/main/java/com/managepro/ui/ComboBox.java
@@ -1,15 +1,15 @@
 package com.managepro.ui;
 
 import java.awt.Cursor;
+
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JLabel;
+import javax.swing.JComboBox;
 
-public class Label extends JLabel {
-    
-    public Label(String text) {
-        super(text);
-        
+public class ComboBox<E> extends JComboBox<E> {
+	public ComboBox() {
+		
+		super();
         addMouseListener(new MouseAdapter() {
             public void mouseEntered(MouseEvent e) {
                 setCursor(new Cursor(Cursor.HAND_CURSOR));
