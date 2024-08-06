2e2f8684ac501baa45ad2d15cfe9e8d503acd31a
Vitor Ramgund <vtwolmer9@gmail.com>
2024-08-06 15:02:03 -0300

    Atualizacao da Classe Admin

--- ManagePro/src/main/java/com/managepro/core/model/Admin.java
+++ ManagePro/src/main/java/com/managepro/core/model/Admin.java
@@ -1,22 +1,30 @@
+package teste;
+
+import teste.Funcionario;
+import teste.String;
 
 public class Admin extends Funcionario {
 	
-	private String NivelAcesso;
+	private String nivelAcesso;
 	
-	public Admin(String nome, String senha, String email, String cargo, String dataAdmissao, int frequencia, String NivelAcesso);
-		super(nome, senha, email, cargo, dataAdmissao, frequencia, nivel acesso);
-		
-		public Admin() {
-			super();
-		}
-		
-		public String NivelAcesso() {
-			return NivelAcesso;
-		}
-		
-		public void setNivelAcesso(String NivelAcesso) {
-			this.NivelAcesso = NivelAcesso;
-		}
+	public Admin(String nome, String email, String cpf, String nivelAcesso) {
+		super(nome, email, cpf);
+		this.nivelAcesso = nivelAcesso;
+	}
+	
+	public Admin() {
+		super();
+	}
+
+	public String getNivelAcesso() {
+		return nivelAcesso;
+	}
+
+	public void setNivelAcesso(String nivelAcesso) {
+		this.nivelAcesso = nivelAcesso;
+	}
+	
+	
 	
 	
 }
