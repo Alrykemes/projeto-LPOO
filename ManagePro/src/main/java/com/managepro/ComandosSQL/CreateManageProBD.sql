bdafc3b80701e7a62ef15ff5b0274d85c355f0b1
CaioCesarMDS <caiocesarsts@gmail.com>
2024-08-13 10:56:42 -0300

    alteração no comando de criação do BD

--- ManagePro/src/main/java/com/managepro/ComandosSQL/CreateManageProBD.sql
+++ ManagePro/src/main/java/com/managepro/ComandosSQL/CreateManageProBD.sql
@@ -1,69 +1,64 @@
 BEGIN;
 
 CREATE TABLE cliente (
-  id_cliente int NOT NULL,
+  id_cliente INT PRIMARY KEY NOT NULL,
   nome varchar(70) NOT NULL,
-  cpf varchar(11) NOT NULL,
-  data_nascimento date NOT NULL,
-  PRIMARY KEY (id_cliente)
+  cpf varchar(13) NOT NULL,
+  data_nascimento DATE NOT NULL
 );
 
 CREATE TABLE funcionario (
-    id_funcionario INT NOT NULL,
+    id_funcionario INT PRIMARY KEY NOT NULL,
     nome VARCHAR(70) NOT NULL,
-    cpf VARCHAR(45) NOT NULL,
-    cargo ENUM("ADMINISTRADOR", "CONTADOR", "ESTOQUISTA", "GERENTE", "VENDEDOR") NOT NULL,,
+    cpf VARCHAR(13) NOT NULL,
+    cargo ENUM("ADMINISTRADOR", "CONTADOR", "ESTOQUISTA", "GERENTE", "VENDEDOR") NOT NULL,
     salario decimal NOT NULL,
     data_admissao DATE NOT NULL,
     usuario varchar(45) NOT NULL,
-    senha varchar(120) NOT NULL,
-    PRIMARY KEY (id_funcionario)
+    senha varchar(120) NOT NULL
 );
 
 CREATE TABLE telefone_cliente (
+	id_telefone_cliente INT PRIMARY KEY NOT NULL, 
     id_cliente INT NOT NULL,
     numero VARCHAR(15) NOT NULL,
-    FOREIGN KEY (id_cliente)
-        REFERENCES cliente(id_cliente)
+    FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
 );
 
 CREATE TABLE telefone_funcionario (
-    id_cliente INT NOT NULL,
+	id_telefone_funcionario INT PRIMARY KEY NOT NULL,
+    id_funcionario INT NOT NULL,
     numero VARCHAR(15) NOT NULL,
-    FOREIGN KEY (id_funcionario)
-        REFERENCES funcionario(id_funcionario)
+    FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario)
 );
 
 CREATE TABLE produto (
-  id_produto int NOT NULL,
-  nome varchar(45) NOT NULL,
-  preco decimal NOT NULL,
-  quantidade int NOT NULL,
-  marca varchar(45) NOT NULL,
-  fornecedor varchar(45) NOT NULL,
-  validade date NOT NULL,
-  PRIMARY KEY (id_produto)
+  id_produto INT PRIMARY KEY NOT NULL,
+  nome VARCHAR(45) NOT NULL,
+  preco DECIMAL NOT NULL,
+  quantidade INT NOT NULL,
+  marca VARCHAR(45) NOT NULL,
+  fornecedor VARCHAR(45) NOT NULL,
+  validade DATE NOT NULL
 );
 
 CREATE TABLE venda (
-  id_venda int NOT NULL,
-  id_funcionario int NOT NULL,
-  id_cliente int NOT NULL,
+  id_venda INT PRIMARY KEY NOT NULL,
+  id_funcionario INT NOT NULL,
+  id_cliente INT NOT NULL,
   forma_pagamento ENUM("CARTAODEALIMENTACAO", "CARTAODECREDITO", "CARTAODEDEBITO", "DINHEIRO", "PIX") NOT NULL,
-  data_venda date NOT NULL,
-  preco decimal NOT NULL,
-  PRIMARY KEY (id_venda),
+  data_venda DATE NOT NULL,
+  preco DECIMAL NOT NULL,
   FOREIGN KEY (id_funcionario) REFERENCES funcionario(id_funcionario),
   FOREIGN KEY (id_cliente) REFERENCES cliente(id_cliente)
   );
 
 CREATE TABLE produto_venda (
-  id_produto_venda int NOT NULL,
-  id_venda int NOT NULL,
-  id_produto int NOT NULL,
-  quantidade int NOT NULL,
-  preco decimal NOT NULL,
-  PRIMARY KEY (id_produto_venda),
+  id_produto_venda INT PRIMARY KEY NOT NULL,
+  id_venda INT NOT NULL,
+  id_produto INT NOT NULL,
+  quantidade INT NOT NULL,
+  preco DECIMAL NOT NULL,
   FOREIGN KEY (id_venda) REFERENCES venda(id_venda),
   FOREIGN KEY (id_produto) REFERENCES produto(id_produto)
 );
